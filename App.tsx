f78d08c6949a409e0f88da9a3b6d3e6b3f88f9c1
Carlos Loureda <balanzeneto@gmail.com>
2020-03-07 15:38:49 +0100

    feat: Added 4 basic screens
    Also added react-native-elements

--- App.tsx
+++ App.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { Button, ThemeProvider } from "react-native-elements";
+import Restaurant from "./components/screens/Restaurants";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <Button title="Hey!"></Button>
+      <Restaurant />
     </View>
   );
 }
@@ -12,8 +16,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
